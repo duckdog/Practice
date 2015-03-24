3ce9d3993e3d5232d43e1445718f230887475139
tom10987 <tom10987@gmail.com>
2015-03-24 17:09:44 +0900

    タイトルロゴ変更

--- Mario_Copy/src/Origin/Scene/title.cpp
+++ Mario_Copy/src/Origin/Scene/title.cpp
@@ -3,15 +3,45 @@
 
 
 cTitle::cTitle(Font& font) :
-cScene(font) {
+cScene(font),
+m_blink(0) {
+
+  const std::string StringData[] = {
+    "タイトル",
+    "スタートするボタン",
+  };
+
+  for (auto& it : StringData) {
+    m_string.emplace_back(it);
+  }
 }
 
 
 void cTitle::update() {
+  ++m_blink;
+  if (m_blink >= 60) { m_blink = 0; }
 }
 
 
 void cTitle::draw() {
-  m_font.draw("下水道の戦い（仮）", Vec2f(0, 0), Color::white);
-  m_font.draw("～泥沼編～", Vec2f(0, -50), Color::yellow);
+  titleLogo();
+  startButton();
+}
+
+
+void cTitle::titleLogo() {
+  const std::string& Logo = m_string[TitleLogo];
+  const float LogoPosX = -m_font.drawSize(Logo).x() / 2;
+  const float LogoPosY = common::HEIGHT / 2 - m_font.drawSize(Logo).y() * 4;
+
+  m_font.draw(Logo, Vec2f(LogoPosX, LogoPosY), Color::white);
+}
+
+
+void cTitle::startButton() {
+  const std::string& Button = m_string[StartButton];
+  const float StringPosX = -m_font.drawSize(Button).x() / 2;
+  const Color BtnColor = Color(1, 1, 0, (m_blink / 30) % 2);
+
+  m_font.draw(Button, Vec2f(StringPosX, 0), BtnColor);
 }
--- Mario_Copy/src/Origin/Scene/title.h
+++ Mario_Copy/src/Origin/Scene/title.h
@@ -1,6 +1,7 @@
 
 #pragma once
 #include "scene.hpp"
+#include <vector>
 
 
 class cTitle : public cScene {
@@ -13,4 +14,17 @@ public:
 
 
 private:
+
+  enum StringList {
+    TitleLogo,
+    StartButton,
+
+    All_String,
+  };
+  std::vector<std::string> m_string;
+
+  short m_blink;
+
+  void titleLogo();
+  void startButton();
 };
